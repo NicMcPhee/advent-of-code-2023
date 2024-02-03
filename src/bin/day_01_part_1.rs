3e6cc941d0a9bee8bdba363d2b5992ff0b10fee1
Nic McPhee <mcphee@morris.umn.edu>
2024-02-03 14:45:43 -0600

    Solve Day 01, Part 1

--- src/bin/day_01_part_1.rs
+++ src/bin/day_01_part_1.rs
@@ -1,3 +1,18 @@
+fn calibration_value(line: &str) -> u32 {
+    // Filter just the digits in `line`
+    let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+    let first = digits.next().unwrap();
+    let last = digits.next_back().unwrap_or(first);
+    10 * first + last
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Read the input file "day_01_test.txt"
+    // and store it in the variable "input"
+    let input = include_str!("../inputs/day_01.txt");
+    let lines = input.lines();
+
+    let result = lines.map(calibration_value).sum::<u32>();
+
+    println!("Result: {}", result);
 }
