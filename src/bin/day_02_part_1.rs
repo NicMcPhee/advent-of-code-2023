bab500f1e05cc803dbce05665ccac2a696c6d031
Nic McPhee <mcphee@morris.umn.edu>
2024-02-10 15:18:26 -0600

    Solve (brutally) Part 1 of Day 02
    This isn't very pretty and I want to do it possibly multiple other ways to see how we can improve it.

--- src/bin/day_02_part_1.rs
+++ src/bin/day_02_part_1.rs
@@ -1,4 +1,4 @@
-use std::str::Lines;
+use std::collections::HashMap;
 
 use pest::Parser;
 use pest_derive::Parser;
@@ -7,23 +7,43 @@ use pest_derive::Parser;
 #[grammar = "grammars/day_02.pest"]
 struct GameParser;
 
-// Returns the game number if this game is legal
-fn legal_game(line: &str) -> Option<u32> {
-    let game = GameParser::parse(Rule::game, line).unwrap();
-    todo!()
-}
+fn sum_of_legal_game_ids(input: &str) -> u32 {
+    let mut result = 0;
+    let max_count = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+    let games = GameParser::parse(Rule::input, input).unwrap();
+    for game in games {
+        let Rule::game = game.as_rule() else {
+            panic!("Expected a game, but got {:?}", game);
+        };
+        let mut game_components = game.into_inner();
+        let game_number: u32 = game_components.next().unwrap().as_str().parse().unwrap();
+        let mut is_legal = true;
+        for reveal in game_components {
+            for cube_count_pair in reveal.into_inner() {
+                let mut cube_count_iter = cube_count_pair.into_inner();
+                let count: u32 = cube_count_iter.next().unwrap().as_str().parse().unwrap();
+                let color = cube_count_iter.next().unwrap().as_str();
+                if count > *max_count.get(color).unwrap() {
+                    is_legal = false;
+                    break;
+                }
+            }
+            if !is_legal {
+                break;
+            }
+        }
+        if is_legal {
+            result += game_number;
+        }
+    }
 
-fn count_legal_games(lines: Lines) -> u32 {
-    lines.filter_map(legal_game).sum::<u32>()
+    result
 }
 
 fn main() {
-    // Read the input file "day_01_test.txt"
-    // and store it in the variable "input"
-    let input = include_str!("../inputs/day_02_test.txt");
-    let lines = input.lines();
+    let input = include_str!("../inputs/day_02.txt");
 
-    let result = count_legal_games(lines);
+    let result = sum_of_legal_game_ids(input);
 
     println!("Result: {}", result);
 }
