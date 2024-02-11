3b77c5abb0df28ac6f2fac7bdea6dd6aeb89bbdf
Nic McPhee <mcphee@morris.umn.edu>
2024-02-10 20:39:18 -0600

    Rewrite Part 1 of Day 02 to use `pest_consume`
    This changes from the fairly blunt `for` loop approach in the first version to a solution using the `pest_consume` package. This meant we created domain-level structures, which is good.
    
    I find the `impl GameParser` section a wee bit tedious, although that may be mostly a matter of not being used to handling Pest grammars.
    
    That said, It's definitely an improvement on the `for` loop solution.

--- src/bin/day_02_part_1.rs
+++ src/bin/day_02_part_1.rs
@@ -1,49 +1,148 @@
-use std::collections::HashMap;
+use pest_consume::{match_nodes, Error, Parser};
 
-use pest::Parser;
-use pest_derive::Parser;
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+#[derive(Debug)]
+struct Reveal {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+impl Reveal {
+    fn within(&self, max_count: &Reveal) -> bool {
+        self.red <= max_count.red && self.green <= max_count.green && self.blue <= max_count.blue
+    }
+}
+
+type CubeCount = (u32, Color);
+
+impl FromIterator<CubeCount> for Reveal {
+    fn from_iter<I: IntoIterator<Item = CubeCount>>(iter: I) -> Self {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+        for (count, color) in iter {
+            match color {
+                Color::Red => red += count,
+                Color::Green => green += count,
+                Color::Blue => blue += count,
+            }
+        }
+        Reveal { red, green, blue }
+    }
+}
+
+#[derive(Debug)]
+struct Game {
+    number: u32,
+    reveals: Vec<Reveal>,
+}
 
 #[derive(Parser)]
 #[grammar = "grammars/day_02.pest"]
 struct GameParser;
 
-fn sum_of_legal_game_ids(input: &str) -> u32 {
-    let mut result = 0;
-    let max_count = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
-    let games = GameParser::parse(Rule::input, input).unwrap();
-    for game in games {
-        let Rule::game = game.as_rule() else {
-            panic!("Expected a game, but got {:?}", game);
-        };
-        let mut game_components = game.into_inner();
-        let game_number: u32 = game_components.next().unwrap().as_str().parse().unwrap();
-        let mut is_legal = true;
-        for reveal in game_components {
-            for cube_count_pair in reveal.into_inner() {
-                let mut cube_count_iter = cube_count_pair.into_inner();
-                let count: u32 = cube_count_iter.next().unwrap().as_str().parse().unwrap();
-                let color = cube_count_iter.next().unwrap().as_str();
-                if count > *max_count.get(color).unwrap() {
-                    is_legal = false;
-                    break;
-                }
-            }
-            if !is_legal {
-                break;
-            }
-        }
-        if is_legal {
-            result += game_number;
-        }
+type Result<T> = std::result::Result<T, Error<Rule>>;
+type Node<'i> = pest_consume::Node<'i, Rule, ()>;
+
+#[pest_consume::parser]
+impl GameParser {
+    fn input(input: Node) -> Result<Vec<Game>> {
+        Ok(match_nodes!(input.into_children();
+            [game(g)..] => g.collect(),
+        ))
     }
 
-    result
+    fn game(input: Node) -> Result<Game> {
+        Ok(match_nodes!(input.into_children();
+            [int(n), reveal(r)..] => Game { number: n, reveals: r.collect() },
+        ))
+    }
+
+    fn int(input: Node) -> Result<u32> {
+        Ok(input.as_str().parse().unwrap())
+    }
+
+    fn reveal(input: Node) -> Result<Reveal> {
+        Ok(match_nodes!(input.into_children();
+            [cubeCount(c)..] => c.collect::<Reveal>(),
+        ))
+    }
+
+    fn cubeCount(input: Node) -> Result<CubeCount> {
+        Ok(match_nodes!(input.into_children();
+            [int(n), color(c)] => (n, c),
+        ))
+    }
+
+    fn color(input: Node) -> Result<Color> {
+        Ok(match_nodes!(input.into_children();
+            [red(c)] => c, [green(c)] => c, [blue(c)] => c,
+        ))
+    }
+
+    fn red(input: Node) -> Result<Color> {
+        Ok(Color::Red)
+    }
+
+    fn green(input: Node) -> Result<Color> {
+        Ok(Color::Green)
+    }
+
+    fn blue(input: Node) -> Result<Color> {
+        Ok(Color::Blue)
+    }
 }
 
-fn main() {
+fn sum_of_legal_game_ids(input: &str) -> anyhow::Result<u32> {
+    let max_count = Reveal {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+    let games = GameParser::parse(Rule::input, input).unwrap();
+    let games = games.single()?;
+    let games = GameParser::input(games)?;
+    Ok(games
+        .into_iter()
+        .filter_map(|game| {
+            game.reveals
+                .iter()
+                .all(|reveal| reveal.within(&max_count))
+                .then_some(game.number)
+        })
+        .sum())
+}
+
+fn main() -> anyhow::Result<()> {
     let input = include_str!("../inputs/day_02.txt");
-
     let result = sum_of_legal_game_ids(input);
+    println!("Result: {}", result?);
 
-    println!("Result: {}", result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_test_input() {
+        let input = include_str!("../inputs/day_02_test.txt");
+        let result = sum_of_legal_game_ids(input).unwrap();
+        assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn check_full_input() {
+        let input = include_str!("../inputs/day_02.txt");
+        let result = sum_of_legal_game_ids(input).unwrap();
+        assert_eq!(result, 2285);
+    }
 }
