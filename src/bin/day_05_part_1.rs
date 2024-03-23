42105d0834a49b4fe34e189da901f7eb0115bed0
Nic McPhee <mcphee@morris.umn.edu>
2024-03-23 15:19:19 -0500

    Implement Day 05 Part 1
    This adds `MappingType` so we don't have raw `String`s in `Mapping`s. It turns out that we never use them so I added `#[allow(dead_code)]` so I could keep them for now without Clippy being unhappy.

--- src/bin/day_05_part_1.rs
+++ src/bin/day_05_part_1.rs
@@ -3,16 +3,49 @@ use std::{ops::Range, str::FromStr};
 use pest_consume::{match_nodes, Error, Parser};
 
 #[derive(Debug)]
+enum MappingType {
+    Seed,
+    Soil,
+    Fertilizer,
+    Water,
+    Light,
+    Temperature,
+    Humidity,
+    Location,
+}
+
+#[derive(Debug)]
 struct Almanac {
     seeds: Vec<u64>,
     maps: Vec<Mapping>,
 }
 
+impl Almanac {
+    fn convert(&self, value: u64) -> u64 {
+        self.maps.iter().fold(value, |acc, m| m.convert(acc))
+    }
+
+    fn lowest_location(&self) -> Option<u64> {
+        self.seeds.iter().map(|s| self.convert(*s)).min()
+    }
+}
+
 #[derive(Debug)]
 struct Mapping {
-    source: String,
-    target: String,
+    #[allow(dead_code)]
+    source: MappingType,
+    #[allow(dead_code)]
+    target: MappingType,
     ranges: Vec<RangeMapping>,
+}
+
+impl Mapping {
+    fn convert(&self, value: u64) -> u64 {
+        self.ranges
+            .iter()
+            .find_map(|r| r.convert(value))
+            .unwrap_or(value)
+    }
 }
 
 #[derive(Debug)]
@@ -21,18 +54,21 @@ struct RangeMapping {
     offset: i64,
 }
 
+impl RangeMapping {
+    fn convert(&self, value: u64) -> Option<u64> {
+        if !self.range.contains(&value) {
+            return None;
+        }
+        Some((value as i64 + self.offset) as u64)
+    }
+}
+
 impl FromStr for Almanac {
     type Err = Error<Rule>;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         let parts = AlmanacParser::parse(Rule::input, s)?.single()?;
-        Ok(AlmanacParser::input(parts).map_err(Into::into)?)
-    }
-}
-
-impl Almanac {
-    fn lowest_location(&self) -> anyhow::Result<u64> {
-        todo!()
+        AlmanacParser::input(parts).map_err(Into::into)
     }
 }
 
@@ -70,9 +106,9 @@ impl AlmanacParser {
         })
     }
 
-    fn map_title(input: Node) -> Result<(String, String)> {
+    fn map_title(input: Node) -> Result<(MappingType, MappingType)> {
         Ok(match_nodes! { input.into_children();
-            [name(source), name(target)] => (source.as_str().to_string(), target.as_str().to_string()),
+            [mapping_type(source), mapping_type(target)] => (source, target),
         })
     }
 
@@ -85,8 +121,19 @@ impl AlmanacParser {
         })
     }
 
-    fn name(input: Node) -> Result<String> {
-        Ok(input.as_str().to_string())
+    fn mapping_type(input: Node) -> Result<MappingType> {
+        let str = input.as_str();
+        Ok(match str {
+            "seed" => MappingType::Seed,
+            "soil" => MappingType::Soil,
+            "fertilizer" => MappingType::Fertilizer,
+            "water" => MappingType::Water,
+            "light" => MappingType::Light,
+            "temperature" => MappingType::Temperature,
+            "humidity" => MappingType::Humidity,
+            "location" => MappingType::Location,
+            _ => return Err(input.error("Unknown mapping type")),
+        })
     }
 
     fn number(input: Node) -> Result<u64> {
@@ -99,10 +146,9 @@ impl AlmanacParser {
 }
 
 fn main() -> anyhow::Result<()> {
-    let input = include_str!("../inputs/day_05_test.txt");
+    let input = include_str!("../inputs/day_05.txt");
     let almanac = Almanac::from_str(input)?;
-    println!("{almanac:#?}");
-    let result = almanac.lowest_location()?;
+    let result = almanac.lowest_location().expect("No location found");
     println!("Result: {}", result);
 
     Ok(())
@@ -125,6 +171,6 @@ mod day_04_part_1_tests {
         let input = include_str!("../inputs/day_05.txt");
         let almanac = Almanac::from_str(input).unwrap();
         let result = almanac.lowest_location().unwrap();
-        assert_eq!(result, 6420979);
+        assert_eq!(result, 88151870);
     }
 }
