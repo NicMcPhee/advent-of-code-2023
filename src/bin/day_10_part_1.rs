c0350290f866fc9a67094e562130040dac4d092e
Nic McPhee <mcphee@morris.umn.edu>
2024-08-03 17:40:24 -0500

    Finish Part 1 of Day 10
    This took anther 2-hour streaming session plus over 1.5 hours of tidy-up and refactoring after the session ended. Yikes!
    
    It's still not super clean; I think that either going back to `anyhow` or forward to `miette` would improve things a fair bit.

--- src/bin/day_10_part_1.rs
+++ src/bin/day_10_part_1.rs
@@ -1,10 +1,10 @@
-use std::{num::ParseIntError, ops::BitOr, str::FromStr};
-
-use itertools::Itertools;
-use miette::Diagnostic;
+use std::{
+    ops::{Add, BitOr},
+    str::FromStr,
+};
 use strum::{EnumIter, EnumString, FromRepr, IntoEnumIterator};
 
-#[derive(FromRepr, EnumIter, Clone, Copy)]
+#[derive(Debug, FromRepr, EnumIter, Clone, Copy)]
 #[repr(u8)]
 enum Connection {
     North = 0b1000,
@@ -44,7 +44,7 @@ impl BitOr for Connection {
 */
 #[derive(EnumString, FromRepr, Debug, Clone, Copy, PartialEq)]
 #[repr(u8)]
-enum Cell {
+enum CellType {
     #[strum(serialize = "|")]
     NsPipe = b'|',
     #[strum(serialize = "-")]
@@ -66,7 +66,15 @@ enum Cell {
 #[derive(Debug)]
 struct IllegalConnectionError;
 
-impl Cell {
+impl CellType {
+    /// All the directions (`Connection`s) reachable from this cell type,
+    /// represented with bit flags as a `u8`.
+    ///
+    /// `Ground` is 0 because starting from a `Ground` cell we can't reach
+    /// anything.
+    ///
+    /// `Start` is all four directions because we can go anywhere from the
+    /// starting position.
     fn connections(self) -> u8 {
         match self {
             Self::NsPipe => Connection::North | Connection::South,
@@ -75,29 +83,108 @@ impl Cell {
             Self::NwBend => Connection::North | Connection::West,
             Self::SwBend => Connection::South | Connection::West,
             Self::SeBend => Connection::South | Connection::East,
-            Self::Ground | Self::Start => 0,
+            Self::Ground => 0,
+            Self::Start => {
+                // The grouping here is necessary to prevent the evaluation of either
+                // a `u8 | Connection` or `Connection | u8` expression, neither of
+                // which is current supported. We could implement `BitOr` for these
+                // combinations of types, but that seems like overkill at the moment.
+                (Connection::North | Connection::South) | (Connection::East | Connection::West)
+            }
         }
     }
 
     fn connection_from(self, incoming: Connection) -> Result<Connection, IllegalConnectionError> {
-        Connection::from_repr(self.connections() ^ (incoming.reverse() as u8))
+        // This should never be called with `Start` since it won't
+        // actually work in that case.
+        assert_ne!(
+            self,
+            Self::Start,
+            "`connection_from(CellType::Start) doesn't actually work",
+        );
+        // `self.connections()` is all the connections/directions reachable from this point. `Ground`
+        // returns no connections, and `Start` returns all four.
+        //
+        // `incoming.reverse()` is the reverse of the incoming direction, e.g., if we're coming
+        // here by traveling `East`, reversing that will give us `West`.
+        //
+        // The bitwise negation `!incoming.reverse()` gives us all the directions _except_ the
+        // reverse of our incoming direction. So in our example, this would give us north, south,
+        // and west.
+        //
+        // Bitwise & of these will give us anything that's in both. In most cases `self.connections()`
+        // will return two directions, one of which is the one direction not in `!incoming.reverse()`,
+        // so we just get the remaining option, which is the outgoing direction that doesn't take
+        // us back to where we came from. If we're at `Ground` we'll get nothing back since `self.connections()`
+        // will return the "empty set".
+        Connection::from_repr(self.connections() & !(incoming.reverse() as u8))
             .ok_or(IllegalConnectionError)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Pos {
+    row: usize,
+    col: usize,
+}
+
+impl Pos {
+    const fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+}
+
+impl Add<Connection> for Pos {
+    type Output = Result<Self, Self>;
+
+    fn add(self, rhs: Connection) -> Self::Output {
+        let Self { row, col } = self;
+        Ok(match rhs {
+            Connection::North => Self {
+                row: row.checked_sub(1).ok_or(self)?,
+                col,
+            },
+            Connection::East => Self {
+                row,
+                col: col.checked_add(1).ok_or(self)?,
+            },
+            Connection::South => Self {
+                row: row.checked_add(1).ok_or(self)?,
+                col,
+            },
+            Connection::West => Self {
+                row,
+                col: col.checked_sub(1).ok_or(self)?,
+            },
+        })
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Cell {
+    cell_type: CellType,
+    pos: Pos,
+}
+
+impl Cell {
+    pub const fn new(cell_type: CellType, pos: Pos) -> Self {
+        Self { cell_type, pos }
+    }
+
+    pub const fn new_from_coords(cell_type: CellType, row: usize, col: usize) -> Self {
+        Self::new(cell_type, Pos::new(row, col))
+    }
+
+    pub const fn empty(pos: Pos) -> Self {
+        Self::new(CellType::Ground, pos)
     }
 }
 
 #[derive(Debug)]
 struct PipeMap {
     entries: Vec<Vec<Cell>>,
-    start_row: usize,
-    start_col: usize,
+    start: Pos,
 }
-
-// #[derive(thiserror::Error, Debug, Diagnostic)]
-// enum PipeMapParseError {
-//     // #[error("Error parsing a pipe map")]
-//     // #[diagnostic(transparent)]
-//     // ValueHistory(#[from] ValueHistoryParseError),
-// }
 
 #[derive(Debug)]
 struct PipeMapParseError;
@@ -119,48 +206,75 @@ impl FromStr for PipeMap {
                             start_row = Some(row_number);
                             start_col = Some(col_number);
                         };
-                        Cell::from_repr(c).ok_or(PipeMapParseError)
+                        let cell_type = CellType::from_repr(c).ok_or(PipeMapParseError)?;
+                        Ok(Cell::new_from_coords(cell_type, row_number, col_number))
                     })
-                    .collect::<Result<Vec<_>, PipeMapParseError>>()
+                    .collect::<Result<Vec<_>, _>>()
             })
-            .collect::<Result<Vec<_>, PipeMapParseError>>()?;
+            .collect::<Result<Vec<_>, _>>()?;
         let start_row = start_row.ok_or(PipeMapParseError)?;
         let start_col = start_col.ok_or(PipeMapParseError)?;
-        Ok(Self {
-            entries,
-            start_row,
-            start_col,
-        })
+        let start = Pos::new(start_row, start_col);
+        Ok(Self { entries, start })
     }
 }
 
-// TODO: Add a `Pos` struct, and put that in `Cell` so we know where we are.
-
 impl PipeMap {
-    fn move_to(&self, cell: Cell, direction: Connection) -> Cell {
-        match direction {
-            Connection::North => self.entries[],
-            Connection::East => todo!(),
-            Connection::South => todo!(),
-            Connection::West => todo!(),
-        }
-        todo!()
+    fn start_cell(&self) -> Result<Cell, IllegalConnectionError> {
+        self.get(self.start).map_err(|_| IllegalConnectionError)
+    }
+
+    fn starting_options(&self) -> Result<(Cell, Vec<Connection>), IllegalConnectionError> {
+        let start = self.start_cell()?;
+        let start_options = Connection::iter()
+            .filter(|c| {
+                self.advance(start, *c)
+                    .and_then(|cell| cell.cell_type.connection_from(*c))
+                    .is_ok()
+            })
+            .collect::<Vec<_>>();
+        assert_eq!(
+            start_options.len(),
+            2,
+            "We didn't have two options: {start_options:#?}"
+        );
+        Ok((start, start_options))
+    }
+
+    fn get(&self, pos: Pos) -> Result<Cell, Pos> {
+        self.entries
+            .get(pos.row)
+            .and_then(|row| row.get(pos.col))
+            .copied()
+            .ok_or(pos)
+    }
+
+    fn move_to(&self, cell: Cell, direction: Connection) -> Result<Cell, Pos> {
+        self.get((cell.pos + direction)?)
+    }
+
+    fn advance(
+        &self,
+        start: Cell,
+        current_direction: Connection,
+    ) -> Result<Cell, IllegalConnectionError> {
+        // Converting from `Pos` to `IllegalConnectionError` makes the error type match
+        // the type generated by `connection_from`. I feel like this is a place where
+        // using either miette or anyhow would make all this a lot tidier.
+        self.move_to(start, current_direction)
+            .map_err(|_| IllegalConnectionError)
     }
 
     fn half_cycle_length(&self) -> Result<u64, IllegalConnectionError> {
-        let start = self.entries[self.start_row][self.start_col];
-        let start_options = Connection::iter()
-            .filter(|c| start.connection_from(*c).is_ok())
-            .collect::<Vec<_>>();
-        assert_eq!(start_options.len(), 2);
+        let (start, start_options) = self.starting_options()?;
 
         let mut current_direction = start_options[0];
-        let mut current_cell = self.move_to(start, current_direction);
+        let mut current_cell = self.advance(start, current_direction)?;
         let mut num_steps = 1;
 
-        while current_cell != Cell::Start {
-            current_direction = current_cell.connection_from(current_direction)?;
-            current_cell = self.move_to(current_cell, current_direction);
+        while current_cell.cell_type != CellType::Start {
+            current_direction = current_cell.cell_type.connection_from(current_direction)?;
+            current_cell = self.advance(current_cell, current_direction)?;
             num_steps += 1;
         }
 
@@ -169,9 +283,9 @@ impl PipeMap {
 }
 
 fn main() -> Result<(), PipeMapParseError> {
-    let input = include_str!("../inputs/day_10_test_1.txt");
+    let input = include_str!("../inputs/day_10.txt");
     let pipe_map = PipeMap::from_str(input)?;
-    println!("{pipe_map:#?}");
+    // println!("{pipe_map:#?}");
     let result = pipe_map.half_cycle_length();
     println!("Result: {}", result.unwrap());
 
@@ -186,7 +300,7 @@ mod tests {
     fn check_first_test_input() {
         let input = include_str!("../inputs/day_10_test_1.txt");
         let pipe_map = PipeMap::from_str(input).unwrap();
-        let result = pipe_map.half_cycle_length();
+        let result = pipe_map.half_cycle_length().unwrap();
         assert_eq!(result, 4);
     }
 
@@ -194,7 +308,7 @@ mod tests {
     fn check_second_test_input() {
         let input = include_str!("../inputs/day_10_test_2.txt");
         let pipe_map = PipeMap::from_str(input).unwrap();
-        let result = pipe_map.half_cycle_length();
+        let result = pipe_map.half_cycle_length().unwrap();
         assert_eq!(result, 8);
     }
 
@@ -202,7 +316,7 @@ mod tests {
     fn check_full_input() {
         let input = include_str!("../inputs/day_10.txt");
         let pipe_map = PipeMap::from_str(input).unwrap();
-        let result = pipe_map.half_cycle_length();
-        assert_eq!(result, 923);
+        let result = pipe_map.half_cycle_length().unwrap();
+        assert_eq!(result, 6886);
     }
 }
