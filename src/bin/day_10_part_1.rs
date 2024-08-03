6c64bc7c6452e49a85c0b4586b6c95c664123140
Nic McPhee <mcphee@morris.umn.edu>
2024-08-03 13:46:08 -0500

    Set up most of the initial representation and parsing, Day 10
    This does't quite work yet because we don't keep track in `PipeMap` of where we various `Cells` are, so we don't know where we are when we move.

--- src/bin/day_10_part_1.rs
+++ src/bin/day_10_part_1.rs
@@ -2,14 +2,11 @@ use std::{num::ParseIntError, ops::BitOr, str::FromStr};
 
 use itertools::Itertools;
 use miette::Diagnostic;
-use strum::{EnumString, FromRepr};
+use strum::{EnumIter, EnumString, FromRepr, IntoEnumIterator};
 
-// TODO: Either impl bitwise OR for `Connections` or use `BitBags` which will do that for us.
-
-#[derive(FromRepr)]
+#[derive(FromRepr, EnumIter, Clone, Copy)]
 #[repr(u8)]
 enum Connection {
-    Nowhere = 0b0000,
     North = 0b1000,
     East = 0b0100,
     South = 0b0010,
@@ -17,13 +14,12 @@ enum Connection {
 }
 
 impl Connection {
-    fn reverse(&self) -> Self {
+    const fn reverse(self) -> Self {
         match self {
-            Connection::Nowhere => unreachable!("Should never reverse `Nowhere`"),
-            Connection::North => Self::South,
-            Connection::East => Self::West,
-            Connection::South => Self::North,
-            Connection::West => Self::East,
+            Self::North => Self::South,
+            Self::East => Self::West,
+            Self::South => Self::North,
+            Self::West => Self::East,
         }
     }
 }
@@ -46,7 +42,7 @@ impl BitOr for Connection {
    . is ground; there is no pipe in this tile.
    S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 */
-#[derive(EnumString, FromRepr, Debug)]
+#[derive(EnumString, FromRepr, Debug, Clone, Copy, PartialEq)]
 #[repr(u8)]
 enum Cell {
     #[strum(serialize = "|")]
@@ -67,10 +63,11 @@ enum Cell {
     Start = b'S',
 }
 
+#[derive(Debug)]
 struct IllegalConnectionError;
 
 impl Cell {
-    fn connections(&self) -> u8 {
+    fn connections(self) -> u8 {
         match self {
             Self::NsPipe => Connection::North | Connection::South,
             Self::EwPipe => Connection::West | Connection::East,
@@ -78,12 +75,11 @@ impl Cell {
             Self::NwBend => Connection::North | Connection::West,
             Self::SwBend => Connection::South | Connection::West,
             Self::SeBend => Connection::South | Connection::East,
-            Self::Ground => Connection::Nowhere as u8,
-            Self::Start => todo!(),
+            Self::Ground | Self::Start => 0,
         }
     }
 
-    fn connection_from(&self, incoming: Connection) -> Result<Connection, IllegalConnectionError> {
+    fn connection_from(self, incoming: Connection) -> Result<Connection, IllegalConnectionError> {
         Connection::from_repr(self.connections() ^ (incoming.reverse() as u8))
             .ok_or(IllegalConnectionError)
     }
@@ -92,6 +88,8 @@ impl Cell {
 #[derive(Debug)]
 struct PipeMap {
     entries: Vec<Vec<Cell>>,
+    start_row: usize,
+    start_col: usize,
 }
 
 // #[derive(thiserror::Error, Debug, Diagnostic)]
@@ -108,21 +106,65 @@ impl FromStr for PipeMap {
     type Err = PipeMapParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut start_row: Option<usize> = None;
+        let mut start_col: Option<usize> = None;
         let entries = s
             .lines()
-            .map(|line| {
+            .enumerate()
+            .map(|(row_number, line)| {
                 line.bytes()
-                    .map(|c| Cell::from_repr(c).ok_or(PipeMapParseError))
+                    .enumerate()
+                    .map(|(col_number, c)| {
+                        if c == b'S' {
+                            start_row = Some(row_number);
+                            start_col = Some(col_number);
+                        };
+                        Cell::from_repr(c).ok_or(PipeMapParseError)
+                    })
                     .collect::<Result<Vec<_>, PipeMapParseError>>()
             })
             .collect::<Result<Vec<_>, PipeMapParseError>>()?;
-        Ok(Self { entries })
+        let start_row = start_row.ok_or(PipeMapParseError)?;
+        let start_col = start_col.ok_or(PipeMapParseError)?;
+        Ok(Self {
+            entries,
+            start_row,
+            start_col,
+        })
     }
 }
 
+// TODO: Add a `Pos` struct, and put that in `Cell` so we know where we are.
+
 impl PipeMap {
-    fn half_cycle_length(&self) -> i64 {
+    fn move_to(&self, cell: Cell, direction: Connection) -> Cell {
+        match direction {
+            Connection::North => self.entries[],
+            Connection::East => todo!(),
+            Connection::South => todo!(),
+            Connection::West => todo!(),
+        }
         todo!()
+    }
+
+    fn half_cycle_length(&self) -> Result<u64, IllegalConnectionError> {
+        let start = self.entries[self.start_row][self.start_col];
+        let start_options = Connection::iter()
+            .filter(|c| start.connection_from(*c).is_ok())
+            .collect::<Vec<_>>();
+        assert_eq!(start_options.len(), 2);
+
+        let mut current_direction = start_options[0];
+        let mut current_cell = self.move_to(start, current_direction);
+        let mut num_steps = 1;
+
+        while current_cell != Cell::Start {
+            current_direction = current_cell.connection_from(current_direction)?;
+            current_cell = self.move_to(current_cell, current_direction);
+            num_steps += 1;
+        }
+
+        Ok(num_steps / 2)
     }
 }
 
@@ -131,7 +173,7 @@ fn main() -> Result<(), PipeMapParseError> {
     let pipe_map = PipeMap::from_str(input)?;
     println!("{pipe_map:#?}");
     let result = pipe_map.half_cycle_length();
-    println!("Result: {result}");
+    println!("Result: {}", result.unwrap());
 
     Ok(())
 }
