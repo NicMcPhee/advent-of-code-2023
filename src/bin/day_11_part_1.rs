250358a3d1d2eb324a68b68b47d218d72b8e90f6
Nic McPhee <mcphee@morris.umn.edu>
2024-08-17 15:27:37 -0500

    Complete Day 11 Part 1 with pairwise Manhattan distances
    This solves Part 1 of Day 11 using "standard" data structures and computing pairwise Manhattan distances.
    
    Thanks to Vincent_SIFTD@Twitch and @MizardX for lots of help with the adjustments for blank rows and columns in `parse_and_adjust()`.

--- src/bin/day_11_part_1.rs
+++ src/bin/day_11_part_1.rs
@@ -1,16 +1,21 @@
 use std::str::FromStr;
 
+use itertools::Itertools;
 use miette::Diagnostic;
 
 #[derive(Debug)]
 struct Galaxy {
-    x: usize,
-    y: usize,
+    row: usize,
+    col: usize,
 }
 
 impl Galaxy {
-    fn new(x: usize, y: usize) -> Self {
-        Galaxy { x, y }
+    const fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+
+    const fn manhattan_distance(&self, other: &Self) -> usize {
+        self.row.abs_diff(other.row) + self.col.abs_diff(other.col)
     }
 }
 
@@ -20,8 +25,43 @@ struct GalaxyMap {
 }
 
 impl GalaxyMap {
-    fn pairwise_length_sum(&self) -> i64 {
-        todo!()
+    fn parse_and_adjust(s: &str) -> Result<Self, GalaxyMapError> {
+        let galaxy_map: Self = s.parse()?;
+        let mut galaxies = galaxy_map.galaxies;
+
+        // Sort galaxies by x-coordinate
+        galaxies.sort_unstable_by_key(|galaxy| galaxy.row);
+        let mut offset = 0;
+        for i in 1..galaxies.len() {
+            galaxies[i].row += offset;
+            let diff = galaxies[i].row - galaxies[i - 1].row;
+            if diff > 1 {
+                offset += diff - 1;
+                galaxies[i].row += diff - 1;
+            }
+        }
+
+        // Sort galaxies by y-coordinate
+        galaxies.sort_unstable_by_key(|galaxy| galaxy.col);
+        let mut offset = 0;
+        for i in 1..galaxies.len() {
+            galaxies[i].col += offset;
+            let diff = galaxies[i].col - galaxies[i - 1].col;
+            if diff > 1 {
+                offset += diff - 1;
+                galaxies[i].col += diff - 1;
+            }
+        }
+
+        Ok(Self { galaxies })
+    }
+
+    fn pairwise_length_sum(&self) -> usize {
+        self.galaxies
+            .iter()
+            .tuple_combinations()
+            .map(|(p, q)| p.manhattan_distance(q))
+            .sum()
     }
 }
 
@@ -46,9 +86,9 @@ impl FromStr for GalaxyMap {
 }
 
 fn main() -> miette::Result<()> {
-    let input = include_str!("../inputs/day_11_test.txt");
-    let galaxy_map = GalaxyMap::from_str(input)?;
-    println!("{galaxy_map:#?}");
+    let input = include_str!("../inputs/day_11.txt");
+    let galaxy_map = GalaxyMap::parse_and_adjust(input)?;
+    // println!("{galaxy_map:#?}");
     let result = galaxy_map.pairwise_length_sum();
     println!("Result: {result}");
 
@@ -62,17 +102,17 @@ mod tests {
     #[test]
     fn check_test_input() -> Result<(), GalaxyMapError> {
         let input = include_str!("../inputs/day_11_test.txt");
-        let galaxy_map = GalaxyMap::from_str(input)?; // .unwrap();
+        let galaxy_map = GalaxyMap::parse_and_adjust(input)?; // .unwrap();
         let result = galaxy_map.pairwise_length_sum(); // .unwrap();
-        assert_eq!(result, 4);
+        assert_eq!(result, 374);
         Ok(())
     }
 
     #[test]
     fn check_full_input() {
         let input = include_str!("../inputs/day_11.txt");
-        let galaxy_map = GalaxyMap::from_str(input).unwrap();
+        let galaxy_map = GalaxyMap::parse_and_adjust(input).unwrap();
         let result = galaxy_map.pairwise_length_sum();
-        assert_eq!(result, 6886);
+        assert_eq!(result, 10_885_634);
     }
 }
