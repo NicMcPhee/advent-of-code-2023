e008c7122fd041c76bb7145ae70711f793cd627a
Nic McPhee <mcphee@morris.umn.edu>
2024-08-24 15:22:03 -0500

    Parse Day 12
    I made an effort to split the parsing across the types a little more than I have been in the past.

--- src/bin/day_12_part_1.rs
+++ src/bin/day_12_part_1.rs
@@ -1,12 +1,66 @@
-use std::str::FromStr;
+use std::{num::ParseIntError, str::FromStr};
 
 use miette::Diagnostic;
 
 #[derive(Debug, thiserror::Error, Diagnostic)]
-enum ConditionRecordsError {}
+enum ConditionRecordsError {
+    #[error("No space in one of the rows: {0:#?}")]
+    NoSpace(String),
+    #[error("Illegal integer count")]
+    IllegalCount(#[from] ParseIntError),
+    #[error("Illegal character in pattern: {0:#?}")]
+    IllegalPatternChar(char),
+}
 
 #[derive(Debug)]
-struct ConditionRecords {}
+enum Status {
+    Broken,
+    Working,
+    Unknown,
+}
+
+impl TryFrom<char> for Status {
+    type Error = ConditionRecordsError;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        Ok(match value {
+            '#' => Self::Broken,
+            '.' => Self::Working,
+            '?' => Self::Unknown,
+            _ => return Err(ConditionRecordsError::IllegalPatternChar(value)),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct ConditionRecord {
+    pattern: Vec<Status>,
+    counts: Vec<i32>,
+}
+
+impl FromStr for ConditionRecord {
+    type Err = ConditionRecordsError;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let (pattern_chars, counts_chars) = line
+            .split_once(' ')
+            .ok_or_else(|| Self::Err::NoSpace(line.to_string()))?;
+        let pattern: Vec<Status> = pattern_chars
+            .chars()
+            .map(TryInto::try_into)
+            .collect::<Result<_, _>>()?;
+        let counts: Vec<i32> = counts_chars
+            .split(',')
+            .map(str::parse)
+            .collect::<Result<_, _>>()?;
+        Ok(Self { pattern, counts })
+    }
+}
+
+#[derive(Debug)]
+struct ConditionRecords {
+    records: Vec<ConditionRecord>,
+}
 
 impl ConditionRecords {
     fn num_arrangements(&self) -> usize {
@@ -14,16 +68,24 @@ impl ConditionRecords {
     }
 }
 
+impl FromIterator<ConditionRecord> for ConditionRecords {
+    fn from_iter<T: IntoIterator<Item = ConditionRecord>>(iter: T) -> Self {
+        Self {
+            records: iter.into_iter().collect(),
+        }
+    }
+}
+
 impl FromStr for ConditionRecords {
     type Err = ConditionRecordsError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        s.lines().map(str::parse).collect()
     }
 }
 
 fn main() -> miette::Result<()> {
-    let input = include_str!("../inputs/day_11.txt");
+    let input = include_str!("../inputs/day_12_test.txt");
     let condition_records: ConditionRecords = input.parse()?;
     println!("{condition_records:#?}");
     let result = condition_records.num_arrangements();
