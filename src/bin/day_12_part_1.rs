f70a5efd33d7778586fd203ca41637d7e5e5f79d
Nic McPhee <mcphee@morris.umn.edu>
2024-08-25 09:35:28 -0500

    Draft Day 12, Part 1
    This doesn't work, but I think it's probably a small error where we have a 0 somewhere where we should have a 1. The tests for `". 0"` pass, but the tests for `"# 1"` and `"? 1"` fail, returning 0 instead of 1.

--- src/bin/day_12_part_1.rs
+++ src/bin/day_12_part_1.rs
@@ -12,7 +12,7 @@ enum ConditionRecordsError {
     IllegalPatternChar(char),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Status {
     Broken,
     Working,
@@ -35,7 +35,63 @@ impl TryFrom<char> for Status {
 #[derive(Debug)]
 struct ConditionRecord {
     pattern: Vec<Status>,
-    counts: Vec<i32>,
+    counts: Vec<usize>,
+}
+
+impl ConditionRecord {
+    fn num_arrangements(&self) -> usize {
+        println!("About to count arrangements for {self:#?}");
+        self.count_arrangements(0, 0, 0)
+    }
+
+    fn count_arrangements(
+        &self,
+        pattern_pos: usize,
+        counts_pos: usize,
+        broken_count: usize,
+    ) -> usize {
+        println!("{pattern_pos:^3}, {counts_pos:^3}, {broken_count:^3}");
+        // We've reached the end of the counts, but possibly still have patterns to check.
+        // We'll set the current_count (the expected number of broken springs) to 0 since
+        // we've exhausted the counts in `self.counts`. If we see any more broken springs,
+        // that will cause this branch to "fail" and return 0.
+        let current_count = self.counts.get(counts_pos).copied().unwrap_or(0);
+        let status = match self.pattern.get(pattern_pos) {
+            Some(status) => status,
+            // We've exhausted the pattern, but were expecting at least one more broken spring,
+            // so this branch "fails" and we return 0.
+            None if current_count > 0 => return dbg!(0),
+            // We've exhausted the pattern and aren't expecting any more broken springs, so
+            // this part of this branch is successful and we return 1.
+            None => return dbg!(1),
+        };
+        let broken_path = match status {
+            // Adding this broken spring exceeds the expected number in this group,
+            // so this branch "fails" and we return 0.
+            Status::Broken | Status::Unknown if broken_count + 1 > current_count => 0,
+            Status::Broken | Status::Unknown => {
+                self.count_arrangements(pattern_pos + 1, counts_pos, broken_count + 1)
+            }
+            Status::Working => 0,
+        };
+        let working_path = match status {
+            // If we see a working spring, and the current broken spring count doesn't match
+            // the expected broken spring count, then this branch fails and we return 0.
+            Status::Working | Status::Unknown
+                if broken_count > 0 && broken_count != current_count =>
+            {
+                0
+            }
+            Status::Working | Status::Unknown => self.count_arrangements(
+                pattern_pos + 1,
+                counts_pos + usize::from(broken_count > 0),
+                0,
+            ),
+            Status::Broken => 0,
+        };
+        println!("{pattern_pos:^3}, {counts_pos:^3}, {broken_count:^3}, -> {broken_path:^3} + {working_path:^3}");
+        broken_path + working_path
+    }
 }
 
 impl FromStr for ConditionRecord {
@@ -49,7 +105,7 @@ impl FromStr for ConditionRecord {
             .chars()
             .map(TryInto::try_into)
             .collect::<Result<_, _>>()?;
-        let counts: Vec<i32> = counts_chars
+        let counts: Vec<usize> = counts_chars
             .split(',')
             .map(str::parse)
             .collect::<Result<_, _>>()?;
@@ -64,7 +120,10 @@ struct ConditionRecords {
 
 impl ConditionRecords {
     fn num_arrangements(&self) -> usize {
-        todo!()
+        self.records
+            .iter()
+            .map(ConditionRecord::num_arrangements)
+            .sum()
     }
 }
 
@@ -99,10 +158,46 @@ mod tests {
     use super::*;
 
     #[test]
+    fn check_single_dot() -> Result<(), ConditionRecordsError> {
+        let input = ". 0";
+        let condition_records: ConditionRecords = input.parse()?;
+        let result = condition_records.num_arrangements();
+        assert_eq!(result, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn check_single_hash() -> Result<(), ConditionRecordsError> {
+        let input = "# 1";
+        let condition_records: ConditionRecords = input.parse()?;
+        let result = condition_records.num_arrangements();
+        assert_eq!(result, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn check_single_question_zero() -> Result<(), ConditionRecordsError> {
+        let input = "? 0";
+        let condition_records: ConditionRecords = input.parse()?;
+        let result = condition_records.num_arrangements();
+        assert_eq!(result, 1);
+        Ok(())
+    }
+
+    #[test]
+    fn check_single_question_one() -> Result<(), ConditionRecordsError> {
+        let input = "? 1";
+        let condition_records: ConditionRecords = input.parse()?;
+        let result = condition_records.num_arrangements();
+        assert_eq!(result, 1);
+        Ok(())
+    }
+
+    #[test]
     fn check_test_input() -> Result<(), ConditionRecordsError> {
         let input = include_str!("../inputs/day_12_test.txt");
-        let condition_records: ConditionRecords = input.parse()?; // .unwrap();
-        let result = condition_records.num_arrangements(); // .unwrap();
+        let condition_records: ConditionRecords = input.parse()?;
+        let result = condition_records.num_arrangements();
         assert_eq!(result, 21);
         Ok(())
     }
