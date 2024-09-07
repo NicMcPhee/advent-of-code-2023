f95e613a6ed320928e32c80d617bb5eb5e576a70
Nic McPhee <mcphee@morris.umn.edu>
2024-09-07 18:18:49 -0500

    Set up Day 13
    We added the `ndarray` crate to handle the management of the 2D array of locations in each pattern.
    
    We then implemented all the parsing into an `ndarray` `array2`, and that worked out very nicely.
    
    This also adds the test files and sets up the test code using the new types.

---
+++ src/bin/day_13_part_1.rs
@@ -0,0 +1,117 @@
+use miette::Diagnostic;
+use ndarray::{Array, Array2, ShapeError};
+use std::str::FromStr;
+
+#[derive(Debug, Diagnostic, thiserror::Error)]
+enum LavaIslandMapError {
+    #[error("Tried to parse a pattern with no lines")]
+    EmptyPattern,
+
+    #[error(transparent)]
+    ArrayShape(#[from] ShapeError),
+
+    #[error("Illegal location character {0}")]
+    IllegalLocation(char),
+}
+
+#[derive(Debug)]
+enum Location {
+    Ash,
+    Rock,
+}
+
+impl Location {
+    const fn from_char(c: char) -> Result<Self, LavaIslandMapError> {
+        Ok(match c {
+            '.' => Self::Ash,
+            '#' => Self::Rock,
+            c => return Err(LavaIslandMapError::IllegalLocation(c)),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Pattern {
+    array: Array2<Location>,
+}
+
+impl Pattern {
+    fn new(num_columns: usize, locations: Vec<Location>) -> Result<Self, LavaIslandMapError> {
+        debug_assert_eq!(locations.len() % num_columns, 0);
+        let num_rows = locations.len() / num_columns;
+        let array = Array::from_shape_vec((num_rows, num_columns), locations)?;
+        Ok(Self { array })
+    }
+}
+
+impl FromStr for Pattern {
+    type Err = LavaIslandMapError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let num_columns = s
+            .lines()
+            .next()
+            .ok_or(LavaIslandMapError::EmptyPattern)?
+            .len();
+        let locations = s
+            .lines()
+            .flat_map(|s| s.chars().map(Location::from_char))
+            .collect::<Result<Vec<Location>, _>>()?;
+        Self::new(num_columns, locations)
+    }
+}
+
+#[derive(Debug)]
+struct LavaIslandMap {
+    patterns: Vec<Pattern>,
+}
+
+impl FromStr for LavaIslandMap {
+    type Err = LavaIslandMapError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let patterns = s
+            .split("\n\n")
+            .map(Pattern::from_str)
+            .collect::<Result<_, _>>()?;
+        Ok(Self { patterns })
+    }
+}
+
+impl LavaIslandMap {
+    fn reflection_positions(&self) -> usize {
+        todo!()
+    }
+}
+
+fn main() -> miette::Result<()> {
+    let input = include_str!("../inputs/day_13_test.txt");
+    let lava_island_map = LavaIslandMap::from_str(input)?;
+    println!("{lava_island_map:#?}");
+    let result = lava_island_map.reflection_positions();
+    println!("Result: {result}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_test_input() -> Result<(), LavaIslandMapError> {
+        let input = include_str!("../inputs/day_11_test.txt");
+        let lava_island_map = LavaIslandMap::from_str(input)?; // .unwrap();
+        let result = lava_island_map.reflection_positions(); // .unwrap();
+        assert_eq!(result, 405);
+        Ok(())
+    }
+
+    #[test]
+    fn check_full_input() {
+        let input = include_str!("../inputs/day_11.txt");
+        let lava_island_map = LavaIslandMap::from_str(input).unwrap();
+        let result = lava_island_map.reflection_positions();
+        assert_eq!(result, 10_885_634);
+    }
+}
