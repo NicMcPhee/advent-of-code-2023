2d981d9658bb9707aa171eb71588b529f7149a23
Nic McPhee <mcphee@morris.umn.edu>
2024-10-26 16:21:52 -0500

    Set up types and parsing for Day 15, Part 2
    This adds a bunch of new types for Day 15, Part 2 (with some nice use of Rust's type system), and updates the parsing to build these types as appropriate.
    
    In parsing into a `Step`, we used a really cool *spread* trick to match against a slice pattern. I don't think I'd ever used that, and thanks to @MizardX for the suggestion.
    
    We also had to introduce an actual error type (`ParseStepError`) for that parsing since there were now ways that parsing could fail.
    
    We're going to be using a `HashMap<Label, Vec<Lens>>`, so we needed to move the `impl Hash` to `Lens`, and add a custom `impl PartialEq` for `Lens`. We "cheated" and just had `eq()` return `true` all the time, counting on `HashMap` to discriminate different `Label`s based on their hash values.

--- src/bin/day_15_part_2.rs
+++ src/bin/day_15_part_2.rs
@@ -1,20 +1,98 @@
 use std::{
+    collections::HashMap,
     convert::Infallible,
     hash::{BuildHasher, BuildHasherDefault, Hash, Hasher},
     str::FromStr,
 };
+
+use strum::FromRepr;
 
 #[derive(Debug)]
 struct InitializationSequence {
     steps: Vec<Step>,
 }
 
-#[derive(Debug)]
-struct Step(String);
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, FromRepr)]
+enum FocalLength {
+    F1 = 1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+}
 
-impl Hash for Step {
+#[derive(Debug)]
+struct Label(Vec<u8>);
+
+impl PartialEq for Label {
+    fn eq(&self, _: &Self) -> bool {
+        // When used in a `HashMap`, any two `Label`s with the
+        // same hash code should be seen as equal. Here we're just
+        // saying that _all_ `Label`s are equal, and counting on
+        // `HashMap` to discriminate on hash codes
+        // before checking equality.
+        true
+    }
+}
+
+impl From<&[u8]> for Label {
+    fn from(value: &[u8]) -> Self {
+        Self(value.to_vec())
+    }
+}
+
+impl Hash for Label {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        u8::hash_slice(self.0.as_bytes(), state);
+        u8::hash_slice(&self.0, state);
+    }
+}
+
+struct Lens {
+    label: Label,
+    focal_length: FocalLength,
+}
+
+#[derive(Debug)]
+enum Operation {
+    Delete,
+    Insert(FocalLength),
+}
+
+#[derive(Debug)]
+struct Step {
+    label: Label,
+    op: Operation,
+}
+
+#[derive(Debug)]
+enum ParseStepError {
+    InvalidRepresentation(String),
+    IllegalFocalLength(char),
+}
+
+impl FromStr for Step {
+    type Err = ParseStepError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.as_bytes() {
+            [label @ .., b'=', f] => Self {
+                label: label.into(),
+                op: Operation::Insert(
+                    FocalLength::from_repr(*f - b'0')
+                        .ok_or_else(|| ParseStepError::IllegalFocalLength(char::from(*f)))?,
+                ),
+            },
+            [label @ .., b'-'] => Self {
+                label: label.into(),
+                op: Operation::Delete,
+            },
+            _ => return Err(ParseStepError::InvalidRepresentation(s.to_string())),
+        })
     }
 }
 
@@ -37,35 +115,52 @@ impl Hasher for InstructionHasher {
 }
 
 impl InitializationSequence {
-    fn sum_of_hashes(&self) -> u64 {
-        let hasher_builder = BuildHasherDefault::<InstructionHasher>::default();
+    // fn sum_of_hashes(&self) -> u64 {
+    //     let hasher_builder = BuildHasherDefault::<InstructionHasher>::default();
 
-        self.steps
-            .iter()
-            .map(|step| hasher_builder.hash_one(step))
-            .sum()
+    //     self.steps
+    //         .iter()
+    //         .map(|step| hasher_builder.hash_one(step))
+    //         .sum()
+    // }
+
+    fn focusing_power(&self) -> u64 {
+        let hasher_builder = BuildHasherDefault::<InstructionHasher>::default();
+        let boxes = HashMap::<Label, Vec<Lens>, BuildHasherDefault<InstructionHasher>>::with_hasher(
+            hasher_builder,
+        );
+
+        // Loop over instruction sequence, updating the lenses in the boxes
+
+        // Loop over boxes (using the keys of the `HashMap`)
+        //   *Make sure to add one to the box number*
+        //   Loop over lens with indices
+        //     *Make sure to add one to the index*
+        //     Do math
+        //   sum()
+        // sum()
+        todo!()
     }
 }
 
 impl FromStr for InitializationSequence {
-    type Err = Infallible;
+    type Err = ParseStepError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let steps = s
             .trim()
             .split(',')
-            .map(ToOwned::to_owned)
-            .map(Step)
-            .collect::<Vec<_>>();
+            .map(Step::from_str)
+            .collect::<Result<Vec<_>, ParseStepError>>()?;
         Ok(Self { steps })
     }
 }
 
 fn main() {
-    let input = include_str!("../inputs/day_15.txt");
+    let input = include_str!("../inputs/day_15_test.txt");
     let init_seq = InitializationSequence::from_str(input).unwrap();
     // println!("{init_seq:#?}");
-    let result = init_seq.sum_of_hashes();
+    let result = init_seq.focusing_power();
     println!("Result: {result}");
 }
 
@@ -77,7 +172,7 @@ mod tests {
     fn check_day_15_test_input() {
         let input = include_str!("../inputs/day_15_test.txt");
         let init_seq = InitializationSequence::from_str(input).unwrap();
-        let result = init_seq.sum_of_hashes();
+        let result = init_seq.focusing_power();
         assert_eq!(result, 145);
     }
 
@@ -85,7 +180,7 @@ mod tests {
     fn check_day_15_full_input() {
         let input = include_str!("../inputs/day_15.txt");
         let init_seq = InitializationSequence::from_str(input).unwrap();
-        let result = init_seq.sum_of_hashes();
+        let result = init_seq.focusing_power();
         assert_eq!(result, 510_792);
     }
 }
